4b6ff2f52ed7db4ece758f5425e423f82139cf23
paulchou0309 <33887724+paulchou0309@users.noreply.github.com>
2018-05-29 15:36:17 +0800

    Update train_eval_image_classifier.py

--- train_eval_image_classifier.py
+++ train_eval_image_classifier.py
@@ -18,7 +18,7 @@ def parse_args(check=True):
     parser.add_argument('--train_dir', type=str)
     parser.add_argument('--learning_rate', type=float, default=0.001)
    # parser.add_argument('--clone_on_cpu', type=bool, default=True)
-   parser.add_argument('--clone_on_cpu=True')
+  # parser.add_argument('--clone_on_cpu=True')
     parser.add_argument('--optimizer', type=str, default='rmsprop')
     parser.add_argument('--batch_size', type=int, default=32)
 
